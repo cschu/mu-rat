e0d7867ca2e438f0abd11cc623567984054d5f00
Chris Schudoma <cschu1981@gmail.com>
2015-10-05 18:35:56 +0100

    Mutation data is now accessed through a generator. Script now generates heatmap table output.

--- calculate_mutation_ratios.py
+++ calculate_mutation_ratios.py
@@ -62,6 +62,7 @@ def main():
     scaffoldData, homHetRatios = readScaffoldData(sys.argv[1], delimiter=',')
 
     # print each row with data in scaffoldData
+    print '\t'.join(['Marker', 'Chr', 'Start', 'End', 'Pheno', 'Color'])
     for chrBin, row in readMutationData(sys.argv[2], scaffoldData, delimiter=','):
         out = [row[0], chrBin[0], chrBin[1], chrBin[1], row[7], 2 if row[7] == 'het' else 1]
         print '\t'.join(map(str, out))
